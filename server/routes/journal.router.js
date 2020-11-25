3131c3f6cc233f8bfc393f213699a9b01ad69add
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-25 14:44:59 -0600

    rough sketching ideas to POST for journal

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -15,8 +15,41 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const taskItemOne = req.body.taskItemOne;
+  const taskItemTwo = req.body.taskItemTwo;
+  const completeStatusItemOne = req.body.completeStatusItemOne;
+  const completeStatusItemTwo = req.body.completeStatusItemTwo;
+  const notesItemOne = req.body.notesItemOne;
+  const notesItemTwo = req.body.notesItemTwo;
+  const date = req.body.date;
+
+  const queryText = `WITH ins AS (
+    INSERT INTO "journal" ("task_item", "complete_status", "notes", "date")
+    VALUES ($1, $2, $3, $4),
+    ($5, $6, $7, $4) RETURNING id
+  )
+  SELECT array_agg(id) FROM ins;`;
+  const queryArray = [
+    taskItemOne,
+    completeStatusItemOne,
+    notesItemOne,
+    date,
+    taskItemTwo,
+    completeStatusItemTwo,
+    notesItemTwo,
+  ];
+
+  pool
+    .query(queryText, queryArray)
+    .then((dbResponse) => {
+      // console.log(dbResponse.rows[0].array_agg);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('Problem storing Journal entry', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const assignmentRouter = require('./routes/assignment.router');
+const journalRouter = require('./routes/journal.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/assignment', assignmentRouter);
+app.use('/api/journal', journalRouter);
 
 // Serve static files
 app.use(express.static('build'));
