213fd73b86d8a5f73d03c43187907b212fcac185
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-27 13:56:24 -0600

    adding preliminary elements to Student Details Page and Grid layout with material-ui

--- src/pages/StudentDetailsPage/StudentDetailsPage.js
+++ src/pages/StudentDetailsPage/StudentDetailsPage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+// MATERIAL-UI
+import { Container, Grid } from '@material-ui/core';
+
 class StudentDetailsPage extends Component {
   componentDidMount() {
     // this is the id to send in dispatch to get the details for the individual student!
@@ -13,12 +16,39 @@ class StudentDetailsPage extends Component {
   }
 
   render() {
-    // const student = this.props.store.student.studentDetailsReducer;
+    const studentToDisplay = this.props.store.student.studentDetailsReducer;
     return (
-      <div>
+      <Container>
         <h1>STUDENT DETAILS PAGE!</h1>
-        <h2>{this.props.store.student.studentDetailsReducer.first_name}</h2>
-      </div>
+        <Grid container>
+          <Grid item xs={4}>
+            {studentToDisplay.profile_picture ? (
+              <p>PROFILE PICTURE</p>
+            ) : (
+              <img
+                src={process.env.PUBLIC_URL + '/blank-profile-picture.png'}
+                alt="not found"
+                className="blankProfileImg"
+              />
+            )}
+            <h3>{studentToDisplay.first_name}</h3>
+            <p>{studentToDisplay.instrument}</p>
+          </Grid>
+          <Grid item xs={8}>
+            <h2>Next Lesson:</h2>
+            <Grid container>
+              <Grid item xs={6}>
+                <h2>Current Assignment</h2>
+                <p>Lorem Ipsum</p>
+              </Grid>
+              <Grid item xs={6}>
+                <h2>Notes</h2>
+                <p>Lorem Ipsum</p>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     );
   }
 }
