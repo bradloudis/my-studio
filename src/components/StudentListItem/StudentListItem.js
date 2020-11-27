ed13194b597419427ab5cbcb43b6f4f9ea4cbdd0
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-27 10:10:05 -0600

    adding source path for blank profile picture. it needs a max-width

--- src/components/StudentListItem/StudentListItem.js
+++ src/components/StudentListItem/StudentListItem.js
@@ -4,6 +4,14 @@ class StudentListItem extends Component {
   render() {
     return (
       <div>
+        {this.props.student.profile_picture ? (
+          <p>PROFILE PICTURE</p>
+        ) : (
+          <img
+            src={process.env.PUBLIC_URL + '/blank-profile-picture.png'}
+            alt="no profile image found"
+          />
+        )}
         <p>{this.props.student.first_name}</p>
       </div>
     );
