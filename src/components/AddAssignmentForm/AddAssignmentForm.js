c54cf6fe1110eee3c197a0a4a58b2afd366f17ba
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-30 13:51:31 -0600

    adding AddAssignmentForm and conditional render which checks to make sure it is a teacher trying to access the Add Assignment Page

---
+++ src/components/AddAssignmentForm/AddAssignmentForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AddAssignmentForm extends Component {
+  render() {
+    return <h1>Add Assignment</h1>;
+  }
+}
+
+export default AddAssignmentForm;
--- src/pages/AddAssignmentPage/AddAssignmentPage.js
+++ src/pages/AddAssignmentPage/AddAssignmentPage.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+// CUSTOM COMPONENTS
+import AddAssignmentForm from '../../components/AddAssignmentForm/AddAssignmentForm';
 
 class AddAssignmentPage extends Component {
   render() {
-    return <h1>Add Assignment</h1>;
+    return (
+      <div>
+        {this.props.store.user.access_level_id === 1 ? (
+          <AddAssignmentForm />
+        ) : (
+          <h1>Hey, you don't belong here!</h1>
+        )}
+      </div>
+    );
   }
 }
 
-export default AddAssignmentPage;
+export default connect(mapStoreToProps)(AddAssignmentPage);
