2e69aab450eccb589d547c800b81276860ee36fd
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-01 12:11:58 -0600

    adding to GET route for all journal entries

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -4,12 +4,25 @@ const router = express.Router();
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
+const { query } = require('../modules/pool');
 
 /**
  * GET route handles getting ALL journals for a specific student
  */
 router.get('/all', (req, res) => {
-  // GET route code here
+  const queryText = `SELECT * FROM "journal"
+  JOIN "assignment" ON "journal".assignment_id="assignment".id
+  WHERE "journal".user_id=$1;`;
+
+  pool
+    .query(queryText, [req.user.id])
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log('could not get all journals', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
