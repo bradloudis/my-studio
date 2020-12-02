83456ca316870a89e41b602679915ab3e28eb79c
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-02 08:58:07 -0600

    checking match.params.tempKey with a console log

--- src/pages/RegisterStudentPage/RegisterStudentPage.js
+++ src/pages/RegisterStudentPage/RegisterStudentPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class RegisterStudentPage extends Component {
+  componentDidMount() {
+    console.log(this.props.match.params.tempKey);
+  }
   render() {
     return <h1>Finish Registration</h1>;
   }
