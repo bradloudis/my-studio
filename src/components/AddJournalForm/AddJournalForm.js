2c89ee4b461cf3d578bae04c598062d8e35dfeb4
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-03 13:21:18 -0600

    adding drop down selects

--- src/components/AddJournalForm/AddJournalForm.js
+++ src/components/AddJournalForm/AddJournalForm.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 
+// MATERIAL UI
+import {
+  Button,
+  Container,
+  Grid,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormControl,
+} from '@material-ui/core';
+
 class AddJournalForm extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <h2>JOURNAL FORM</h2>
-        <p>
-          <strong>Did you?</strong>
-        </p>
-        <p>
-          {this.props.assignment[0] != null &&
-            this.props.assignment[0].task_item}
-        </p>
-        <p>
-          <strong>Did you?</strong>
-        </p>
-        <p>
-          {this.props.assignment[1] != null &&
-            this.props.assignment[1].task_item}
-        </p>
-      </div>
+        <Grid container spacing={2}>
+          <Grid item>
+            <p>
+              <strong>Did you?</strong>
+            </p>
+            <p>
+              {this.props.assignment[0] != null &&
+                this.props.assignment[0].task_item}
+            </p>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <InputLabel>yes/no</InputLabel>
+              <Select>
+                <MenuItem value={true}>yes</MenuItem>
+                <MenuItem value={false}>no</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item>
+            <p>
+              <strong>Did you?</strong>
+            </p>
+            <p>
+              {this.props.assignment[1] != null &&
+                this.props.assignment[1].task_item}
+            </p>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <InputLabel>yes/no</InputLabel>
+              <Select>
+                <MenuItem value={true}>yes</MenuItem>
+                <MenuItem value={false}>no</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Container>
     );
   }
 }
