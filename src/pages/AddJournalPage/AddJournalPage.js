be9f4ac579705c72e47b5621e29a8593dd9c9cbb
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-03 11:50:53 -0600

    adding Grid to AddJournalPage and stubbing in <h2>JOURNAL FORM</h2>

--- src/pages/AddJournalPage/AddJournalPage.js
+++ src/pages/AddJournalPage/AddJournalPage.js
@@ -6,7 +6,7 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import CurrentAssignment from '../../components/CurrentAssignment/CurrentAssignment';
 
 // MATERIAL UI
-import { Container, Box } from '@material-ui/core';
+import { Container, Box, Grid } from '@material-ui/core';
 
 class AddJournalPage extends Component {
   componentDidMount() {
@@ -19,7 +19,14 @@ class AddJournalPage extends Component {
         {this.props.store.user.access_level_id === 2 ? (
           <Box>
             <h1>Add Journal Entry</h1>
-            <CurrentAssignment />
+            <Grid container>
+              <Grid item xs={3}>
+                <CurrentAssignment />
+              </Grid>
+              <Grid item xs={9}>
+                <h2>JOURNAL FORM</h2>
+              </Grid>
+            </Grid>
           </Box>
         ) : (
           <h1>Hey, you don't belong here!</h1>
