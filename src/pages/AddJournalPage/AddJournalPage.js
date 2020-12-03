2d9a3f45e19b21323c58ad6a292811176e5049ab
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-03 11:46:40 -0600

    adding componentDidMount which dispatches to get assignment. CurrentAssignment displays on page

--- src/pages/AddJournalPage/AddJournalPage.js
+++ src/pages/AddJournalPage/AddJournalPage.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+// CUSTOM COMPONENTS
+import CurrentAssignment from '../../components/CurrentAssignment/CurrentAssignment';
+
+// MATERIAL UI
+import { Container, Box } from '@material-ui/core';
+
 class AddJournalPage extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: 'GET_ASSIGNMENT_STUDENT' });
+  }
+
   render() {
     return (
-      <div>
+      <Container>
         {this.props.store.user.access_level_id === 2 ? (
-          <h1>Add Journal Entry</h1>
+          <Box>
+            <h1>Add Journal Entry</h1>
+            <CurrentAssignment />
+          </Box>
         ) : (
           <h1>Hey, you don't belong here!</h1>
         )}
-      </div>
+      </Container>
     );
   }
 }
