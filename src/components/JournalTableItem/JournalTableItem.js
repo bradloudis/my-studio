d9de8d8181bac53e955b5beacb5785f9a0d30ae0
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-02 20:28:50 -0600

    adding click listener to journal table row

--- src/components/JournalTableItem/JournalTableItem.js
+++ src/components/JournalTableItem/JournalTableItem.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
+// MATERIAL UI
 import { TableRow, TableCell } from '@material-ui/core';
 
 class JournalTableItem extends Component {
+  journalItemClick = () => {
+    console.log('journal item click');
+  };
+
   render() {
     return (
-      <TableRow>
+      <TableRow onClick={this.journalItemClick}>
         <TableCell align="center">{this.props.journalItem.date}</TableCell>
         <TableCell align="center">{this.props.journalItem.notes}</TableCell>
         <TableCell align="center"></TableCell>
@@ -14,4 +20,4 @@ class JournalTableItem extends Component {
   }
 }
 
-export default JournalTableItem;
+export default withRouter(JournalTableItem);
