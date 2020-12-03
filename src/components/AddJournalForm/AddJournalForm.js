eef07cb28e32f9a895af2c92251accb96ca9d312
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-03 13:55:19 -0600

    adding TextField and handlechange function ... function is a bit D.R.Y. but i can condense later

--- src/components/AddJournalForm/AddJournalForm.js
+++ src/components/AddJournalForm/AddJournalForm.js
@@ -11,6 +11,7 @@ import {
   InputLabel,
   MenuItem,
   FormControl,
+  TextField,
 } from '@material-ui/core';
 
 class AddJournalForm extends Component {
@@ -21,6 +22,12 @@ class AddJournalForm extends Component {
   };
 
   handleSelectChange = (propertyName) => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  };
+
+  handleInputChangeFor = (propertyName) => (event) => {
     this.setState({
       [propertyName]: event.target.value,
     });
@@ -41,50 +48,63 @@ class AddJournalForm extends Component {
     return (
       <Container>
         <h2>JOURNAL FORM</h2>
-        <Grid container spacing={2}>
+        <Grid container spacing={3}>
           <Grid item>
-            <p>
-              <strong>Did you?</strong>
-            </p>
-            <p>
-              {this.props.assignment[0] != null &&
-                this.props.assignment[0].task_item}
-            </p>
+            <Grid container spacing={2}>
+              <Grid item>
+                <p>
+                  <strong>Did you?</strong>
+                </p>
+                <p>
+                  {this.props.assignment[0] != null &&
+                    this.props.assignment[0].task_item}
+                </p>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <InputLabel>yes/no</InputLabel>
+                  <Select
+                    value={this.state.taskItemOneBool}
+                    onChange={this.handleSelectChange('taskItemOneBool')}
+                  >
+                    <MenuItem value={true}>yes</MenuItem>
+                    <MenuItem value={false}>no</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item>
+                <p>
+                  <strong>Did you?</strong>
+                </p>
+                <p>
+                  {this.props.assignment[1] != null &&
+                    this.props.assignment[1].task_item}
+                </p>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <InputLabel>yes/no</InputLabel>
+                  <Select
+                    value={this.state.taskItemTwoBool}
+                    onChange={this.handleSelectChange('taskItemTwoBool')}
+                  >
+                    <MenuItem value={true}>yes</MenuItem>
+                    <MenuItem value={false}>no</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item>
-            <FormControl>
-              <InputLabel>yes/no</InputLabel>
-              <Select
-                value={this.state.taskItemOneBool}
-                onChange={this.handleSelectChange('taskItemOneBool')}
-              >
-                <MenuItem value={true}>yes</MenuItem>
-                <MenuItem value={false}>no</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid item>
-            <p>
-              <strong>Did you?</strong>
-            </p>
-            <p>
-              {this.props.assignment[1] != null &&
-                this.props.assignment[1].task_item}
-            </p>
-          </Grid>
-          <Grid item>
-            <FormControl>
-              <InputLabel>yes/no</InputLabel>
-              <Select
-                value={this.state.taskItemTwoBool}
-                onChange={this.handleSelectChange('taskItemTwoBool')}
-              >
-                <MenuItem value={true}>yes</MenuItem>
-                <MenuItem value={false}>no</MenuItem>
-              </Select>
-            </FormControl>
+            <TextField
+              label="note"
+              multiline
+              rows={4}
+              value={this.state.notes}
+              onChange={this.handleInputChangeFor('notes')}
+            />
           </Grid>
         </Grid>
         <Button variant="contained" onClick={this.handleClickSubmit}>
