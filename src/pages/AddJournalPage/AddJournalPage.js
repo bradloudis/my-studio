fe5e839d98951a87707cb493b2ac3ee268c83594
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-03 21:14:45 -0600

    adding back btn navigates to /journal

--- src/pages/AddJournalPage/AddJournalPage.js
+++ src/pages/AddJournalPage/AddJournalPage.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import { withRouter } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
 import CurrentAssignment from '../../components/CurrentAssignment/CurrentAssignment';
 import AddJournalForm from '../../components/AddJournalForm/AddJournalForm';
 
 // MATERIAL UI
-import { Container, Box, Grid } from '@material-ui/core';
+import { Container, Box, Grid, Button } from '@material-ui/core';
 
 class AddJournalPage extends Component {
   componentDidMount() {
     this.props.dispatch({ type: 'GET_ASSIGNMENT_STUDENT' });
   }
 
+  handleBackClick = () => {
+    this.props.history.push('/journal');
+  };
   render() {
     return (
       <Container>
         {this.props.store.user.access_level_id === 2 ? (
           <Box>
             <h1>Add Journal Entry</h1>
+            <Box mb={2}>
+              <Button variant="contained" onClick={this.handleBackClick}>
+                BACK
+              </Button>
+            </Box>
             <Grid container spacing={2}>
               <Grid item xs={3}>
                 <CurrentAssignment />
@@ -37,4 +46,4 @@ class AddJournalPage extends Component {
   }
 }
 
-export default connect(mapStoreToProps)(AddJournalPage);
+export default withRouter(connect(mapStoreToProps)(AddJournalPage));
