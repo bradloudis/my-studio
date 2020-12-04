bd449333ff9de91bf6256046546dccec13e26ce3
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-04 17:22:05 -0600

    failed first attempt at adding hover to table rows

--- src/components/JournalTableItem/JournalTableItem.js
+++ src/components/JournalTableItem/JournalTableItem.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 // MATERIAL UI
-import { TableRow, TableCell, Button } from '@material-ui/core';
+import { TableRow, TableCell } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
 
 //import for date/time config
 import { DateTime } from 'luxon';
+
+const StyledTableRow = withStyles((theme) => ({
+  hover: {
+    backgroundColor: indigo,
+    color: theme.palette.common.white,
+  },
+}))(TableRow);
 
 class JournalTableItem extends Component {
   journalItemClick = () => {
@@ -20,10 +29,10 @@ class JournalTableItem extends Component {
     const date = DateTime.fromISO(this.props.journalItem.date);
     const humanDate = date.toLocaleString(DateTime.DATE_SHORT);
     return (
-      <TableRow onClick={this.journalItemClick}>
+      <StyledTableRow onClick={this.journalItemClick}>
         <TableCell align="center">{humanDate}</TableCell>
         <TableCell align="center">{this.props.journalItem.notes}</TableCell>
-      </TableRow>
+      </StyledTableRow>
     );
   }
 }
