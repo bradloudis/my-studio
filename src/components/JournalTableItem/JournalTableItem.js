37bb1c614c46d7b4f8638b359c3cecc30f2b0b47
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-04 10:27:25 -0600

    adding Luxon to handle rendering dates on journal overview

--- src/components/JournalTableItem/JournalTableItem.js
+++ src/components/JournalTableItem/JournalTableItem.js
@@ -6,6 +6,9 @@ import { TableRow, TableCell, Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
+//import for date/time config
+const { DateTime } = require('luxon');
+
 class JournalTableItem extends Component {
   journalItemClick = () => {
     console.log('journal item click', this.props.journalItem.id);
@@ -18,9 +21,11 @@ class JournalTableItem extends Component {
   };
 
   render() {
+    const date = DateTime.fromISO(this.props.journalItem.date);
+    const humanDate = date.toLocaleString(DateTime.DATE_SHORT);
     return (
       <TableRow onClick={this.journalItemClick}>
-        <TableCell align="center">{this.props.journalItem.date}</TableCell>
+        <TableCell align="center">{humanDate}</TableCell>
         <TableCell align="center">{this.props.journalItem.notes}</TableCell>
         <TableCell align="center">
           <Button>
