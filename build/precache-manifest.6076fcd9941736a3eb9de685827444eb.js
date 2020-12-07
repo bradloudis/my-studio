8148e8a0721956050d11db71764e26038b2d22fe
Brad Loudis <Bradley.Loudis@gmail.com>
2020-12-07 16:26:02 -0600

    production build for deployment

---
+++ build/precache-manifest.6076fcd9941736a3eb9de685827444eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080f1b4b3116649b13c772d2ece944bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eba111676e10ec413df",
+    "url": "/static/css/main.36428e66.chunk.css"
+  },
+  {
+    "revision": "c30f80f764774d28a3ed",
+    "url": "/static/js/2.5d17cbd9.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.5d17cbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eba111676e10ec413df",
+    "url": "/static/js/main.c9e1f1ff.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "5e3f59c7a02fc565b27b81cd2a0994c0",
+    "url": "/static/media/my-studio-background.5e3f59c7.jpg"
+  }
+]);
